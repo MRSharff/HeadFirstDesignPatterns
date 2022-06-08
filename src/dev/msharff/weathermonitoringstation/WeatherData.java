607dda8946d9636ea798bb42c2589163ecf5e910
Mathew Sharff <mrsharff@gmail.com>
2022-06-08 10:16:52 -0700

    Set up Chapter 2 WeatherData

---
+++ src/dev/msharff/weathermonitoringstation/WeatherData.java
@@ -0,0 +1,23 @@
+package dev.msharff.weathermonitoringstation;
+
+public class WeatherData {
+    public float getTemperature() {
+        return 0;
+    }
+
+    public float getHumidity() {
+        return 0;
+    }
+
+    public float getPressure() {
+        return 0;
+    }
+
+    /*
+     * This method gets called whenever the
+     * weather measurements have been updated.
+     */
+    public void measurementsChanged() {
+        // your code goes here
+    }
+}
